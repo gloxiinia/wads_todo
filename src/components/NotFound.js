4a72e697c071a1b2c66c391f2c40c6a5a506683c
gloxiinia <rachelanastasia.w@gmail.com>
2023-03-17 23:30:33 +0700

    styled not found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import "../styles/NotFound.css"
+
+import MissionFailed from "../images/mission-failed.png"
+
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className='not-found-container'>
+      <picture>
+          < img className="mission-failed" alt ="Mission Failed" src={MissionFailed}/>
+        </picture>
+
+      <h1>Page Not Found!</h1>
+      
+    </div>
+
   )
 }
 
